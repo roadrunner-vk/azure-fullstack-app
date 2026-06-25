5cc637380db6a0792ff90099972ffa2326c4a722
VenkatV3 <vinay.venkatesh@basf.com>
2026-06-25 14:43:39 +0200

    security: restrict CORS to frontend domain only

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,10 @@ app = FastAPI(title="Todo API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://ca-frontend.ashyocean-77968f26.northeurope.azurecontainerapps.io",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
